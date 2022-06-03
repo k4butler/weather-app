9312f0d9cdf986df967e37b7f7dc4ba7c06ec5a2
k4butler <99455118+k4butler@users.noreply.github.com>
2022-06-02 22:44:50 -0500

    Fixed Favicon Error

--- src/script.js
+++ src/script.js
@@ -84,10 +84,6 @@ function search(city) {
 function cityName(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
-  let h1 = document.querySelector("h1");
-  // console.log(h1);
-  h1.innerHTML = cityName;
-
   search(city);
 }
 
